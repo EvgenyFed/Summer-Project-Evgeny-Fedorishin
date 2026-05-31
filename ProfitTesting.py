03055e499b1ae09ddb23f23790631f8dea4448bb
EvgenyFed <efedorishin15@gmail.com>
2026-05-31 10:44:24 +0400

    replaced percentage returns with log prices to enable memory, also used numpy to simplify the code

--- ProfitTesting.py
+++ ProfitTesting.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
 import time
+import numpy as np
 
 url = "https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv"
 table = pd.read_csv(url)
@@ -16,51 +17,22 @@ for i in range(len(table)): #reformats tickers so that they can be used by yfina
 tickers = fixed_tickers
 
 
-
 end_date = datetime.today().strftime("%Y-%m-%d") 
 start_date = (datetime.today() - timedelta(days=365)).strftime("%Y-%m-%d") #makes start data exactly a year ago from now
 
 
-
 def profitTesting(ticker1, ticker2, start_date, end_date, time): #calculates the z-score given two tickers and a time period between buying and selling
     prices = yf.download([ticker1, ticker2], start=start_date, end=end_date, auto_adjust=True)["Close"]
-    returns1 = []
-    returns2 = []
 
-    for i in range(1, len(prices)): #For each day, calculate the percentage price change for both stocks and store them in two lists.
-        returns1.append((prices[ticker1].iloc[i] - prices[ticker1].iloc[i-1]) / prices[ticker1].iloc[i-1])
-        returns2.append((prices[ticker2].iloc[i] - prices[ticker2].iloc[i-1]) / prices[ticker2].iloc[i-1])
+    logPrices1 = np.log(prices[ticker1].dropna())
+    logPrices2 = np.log(prices[ticker2].dropna())
 
-    numeratorSum = 0
-    denominatorSum = 0
+    hedgeRatio = (logPrices1 * logPrices2).sum() / (logPrices2 ** 2).sum()
 
-    for z in range(len(returns1)): #calculates hedgeratio between the tickers
-        numeratorSum = (returns1[z] * returns2[z]) + numeratorSum
-        denominatorSum = (returns2[z] ** 2) + denominatorSum
+    spreads = (logPrices1 - hedgeRatio * logPrices2).tolist()
 
-    hedgeRatio = numeratorSum / denominatorSum
-
-    spreads =[]
-
-    for z in range(0, len(returns1)): #makes a list of all the daily spreads
-        spread = returns1[z] - hedgeRatio * returns2[z]
-        spreads.append(spread)
-
-    total = 0
-
-    for spread in spreads: 
-        total = total + spread
-    
-    avgSpread = total / len(spreads)
-
-    varianceNumerator = 0
-
-    for spread in spreads:
-        varianceNumerator = varianceNumerator +((spread - avgSpread) **2)
-    
-    variance = varianceNumerator / len(spreads)
-
-    standardDeviation = variance ** 0.5
+    avgSpread = np.mean(spreads)
+    standardDeviation = np.std(spreads) 
 
     positiveZScores = {}
     negativeZScores = {}
@@ -78,21 +50,17 @@ def profitTesting(ticker1, ticker2, start_date, end_date, time): #calculates the
         if key + time >= len(prices):
             continue
 
-        sellingPrice = prices[ticker1].iloc[key] # shorts stock 1 for a week
+        sellingPrice = prices[ticker1].iloc[key] # shorts stock 1
         buyingPrice = prices[ticker1].iloc[key+time]
         profit1 = (sellingPrice - buyingPrice) / sellingPrice
 
-        buyingPrice = prices[ticker2].iloc[key] # goes long on stock 2 for a week
+        buyingPrice = prices[ticker2].iloc[key] # goes long on stock 2
         sellingPrice = prices[ticker2].iloc[key+time]
         profit2 = ((sellingPrice - buyingPrice) / buyingPrice) * hedgeRatio
 
         totalProfitPos.append(((profit1 + profit2) / (1 + hedgeRatio)) * 100)
 
-    total = 0
-
-    for value in totalProfitPos: # calculates mean profit of buying on each positive z score
-        total = total + value
-    meanProfitPos = total / len(totalProfitPos)
+    meanProfitPos = np.mean(totalProfitPos)
     
     totalProfitNeg = []
 
@@ -100,23 +68,19 @@ def profitTesting(ticker1, ticker2, start_date, end_date, time): #calculates the
         if key + time >= len(prices):
             continue
 
-        buyingPrice = prices[ticker1].iloc[key] # goes long on stock 1 for a week
+        buyingPrice = prices[ticker1].iloc[key] # goes long on stock 1
         sellingPrice = prices[ticker1].iloc[key+time]
         profit1 = (sellingPrice - buyingPrice) / buyingPrice
 
-        sellingPrice = prices[ticker2].iloc[key] # shorts stock 2 for a week
+        sellingPrice = prices[ticker2].iloc[key] # shorts stock 2
         buyingPrice = prices[ticker2].iloc[key+time]
         profit2 = ((sellingPrice - buyingPrice) / sellingPrice) * hedgeRatio
 
         totalProfitNeg.append(((profit1 + profit2) / (1 + hedgeRatio)) * 100)
 
-    total = 0
+    meanProfitNeg = np.mean(totalProfitNeg)
 
-    for value in totalProfitNeg: # calculates mean profit of buying on each negative z score
-        total = total + value
-    meanProfitNeg = total / len(totalProfitNeg)
-
-    return meanProfitPos, meanProfitNeg
+    return float(meanProfitPos), float(meanProfitNeg)
 
 
 
